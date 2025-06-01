e1253687b9dbca977062e94176b66d6775c7bbc7
Muhammet Celik <muhammetclk56@gmail.com>
2025-06-01 12:57:20 +0300

    #Section-2
    Docker compose files added.

--- section2/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
+++ section2/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
@@ -5,6 +5,14 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDto(String message,Map<String,String> contactDetails,List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountContactInfoDto {
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
--- section2/v2-spring-cloud-config/cards/src/main/java/com/eazybytes/cards/dto/CardContactInfoDto.java
+++ section2/v2-spring-cloud-config/cards/src/main/java/com/eazybytes/cards/dto/CardContactInfoDto.java
@@ -5,7 +5,15 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @ConfigurationProperties(prefix = "cards")
-public record CardContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
     
 }
