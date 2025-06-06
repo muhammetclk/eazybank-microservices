696f0cb39a08b5aaf17f6b8ce730f3bfb5523938
Muhammet Celik <muhammetclk56@gmail.com>
2025-06-06 14:40:01 +0300

    #Section-3
    Mysql added.

---
+++ section3/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,33 @@
+package com.eazybytes.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.eazybytes.accounts.dto.AccountContactInfoDto;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@SpringBootApplication
+@EnableConfigurationProperties(value = {AccountContactInfoDto.class})
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+	info=@Info(
+		title="Accounts Microservice Rest APIs Docs",
+	 version="1.0",
+	  description="EazyBank Accounts Microservice",
+	  contact = @Contact(name="Muhammet Celik", email="muhammetclk56@gmail.com", url="https://github.com/muhammetclk"),
+	  license = @License(name="Apache 2.0", url="https://github.com/muhammetclk")),externalDocs = @ExternalDocumentation(description="EazyBank Accounts Rest api docs",url="https://github.com/muhammetclk"))
+
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ section3/cards/src/main/java/com/eazybytes/cards/mapper/CardMapper.java
@@ -0,0 +1,26 @@
+package com.eazybytes.cards.mapper;
+
+import com.eazybytes.cards.dto.CardDto;
+import com.eazybytes.cards.entity.Card;
+
+public class CardMapper {
+    public static CardDto mapToCardsDto(Card card, CardDto cardDto) {
+        cardDto.setCardNumber(card.getCardNumber());
+        cardDto.setCardType(card.getCardType());
+        cardDto.setMobileNumber(card.getMobileNumber());
+        cardDto.setTotalLimit(card.getTotalLimit());
+        cardDto.setAvailableAmount(card.getAvailableAmount());
+        cardDto.setAmountUsed(card.getAmountUsed());
+        return cardDto;
+    }
+
+    public static Card mapToCards(CardDto cardDto, Card card) {
+        card.setCardNumber(cardDto.getCardNumber());
+        card.setCardType(cardDto.getCardType());
+        card.setMobileNumber(cardDto.getMobileNumber());
+        card.setTotalLimit(cardDto.getTotalLimit());
+        card.setAvailableAmount(cardDto.getAvailableAmount());
+        card.setAmountUsed(cardDto.getAmountUsed());
+        return card;
+    }
+}
