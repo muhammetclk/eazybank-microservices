551b3106a2012feef4b77be30fcf23cce0b5470b
Muhammet Celik <muhammetclk56@gmail.com>
2025-06-16 19:02:18 +0300

    #Section-5
    gateway server added

---
+++ section5/gatewayserver/src/test/java/com/eazytbytes/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazytbytes.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
